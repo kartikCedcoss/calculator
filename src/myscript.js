ca5897d043365a82460c9c0596185b7e2ee4936a
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-18 15:47:33 +0530

    correction

--- src/myscript.js
+++ src/myscript.js
@@ -7,10 +7,8 @@ function equal() {
 function clear() {
   $("#display").val("");
 }
-function del() {
-  result = $("#display").val();
-  $("#display").val(result.substring(0, result.length));
-}
+
+
 $("#clear").bind("click", function () {
   $("input[type=text], textarea").val("");
 });
